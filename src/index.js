d2a9a8daf29ec578f3dccfc64b840456301270c9
Frederico <fredericozanoni@gmail.com>
2021-03-10 18:06:02 -0300

    :construction: Testing gitmoji
    test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
 
-
-(async function App() {
-    const main = null || document.getElementById('main');
-    main.innerHTML = await Template();
-})();
+console.log("Test gitmoji")
+    (async function App() {
+        const main = null || document.getElementById('main');
+        main.innerHTML = await Template();
+    })();
